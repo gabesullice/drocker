d88c6449a6733f03a0d25877d8bca7fd639bba07
Gabriel SULLICE <gabriel@sullice.com>
2016-04-21 18:22:53 -0600

    First draft of usable bootstrap

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-read -p "Which version of Drupal will this be for? [8]: " DRUPAL_VERSION
-printf "\n"
-curl -Lo ./Makefile https://raw.githubusercontent.com/gabesullice/drocker/master/bootstrap/Makefile
-make init
+set -e
+
+main () {
+  get_makefile
+  get_drupal_version
+  make init
+}
+
+get_drupal_version () {
+  read -p "Which version of Drupal will this be for? [8]: " DRUPAL_VERSION
+  if ![[ $DRUPAL_VERSION ]]; then DRUPAL_VERSION=8; fi
+  export DRUPAL_VERSION
+  printf "\n"
+}
+
+get_makefile () {
+  curl -Lo ./Makefile https://raw.githubusercontent.com/gabesullice/drocker/master/bootstrap/Makefile
+}
+
+main $@
